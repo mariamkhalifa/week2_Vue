e9e693d8475046f8c7de7505c589e1024b34c86a
mariamkhalifa <m_khalifa@fanhsaweonline.ca>
2020-01-14 16:53:09 -0500

    nav updates

--- js/main.js
+++ js/main.js
@@ -1,23 +1,44 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js';
+// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
 
-const myVM = (() => {
-    let vue_vm = new Vue({
-        data: {
-            message: "Hello from Vue!",
+var vm = new Vue({
+  el: "#app",
 
-            collection: [
-                { name: "Mariam", role:"mom"},
-                { name: "Mohsen", role:"dad"},
-                { name: "Fares", role:"whiney son"},
-                { name: "Amir", role:"the boss"}
-            ]
-        },
+  data: {
 
-        methods: {
-            logClicked() {
-                console.log('clicked on an element!');
-            }
-        }
-    }).$mount("#app");
+    user: {
+      isAdmin: true,
+      isLoggedIn: true,
+      avatar: "thor.png"
+    },
 
-})();
+    // mock up the user - this well eventually come from the database UMS (user management system)
+
+
+    // this data would also come from the database, but we'll just mock it up for now
+    videodata: [
+      { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
+      { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
+      { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
+    ],
+
+    showDetails: false
+  },
+
+  methods: {
+    logInOut() {
+      // tst teh login / logout UI => button should change color
+      // eventually we'll rouing anda login component
+      console.log('do login / logout on click');
+
+      // ? : is a ternanry statement ( shorthand for if / else)
+      // evaluate the espression; if it's true. use the value to the
+      // left of teh semicolon. if it's false, sue the value to teh right
+      this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
+    },
+
+    setUserPrefs() {
+      console.log('set user prefs via routing and probably a component');
+    }
+
+  }
+});
