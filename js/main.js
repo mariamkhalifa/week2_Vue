90d59cd1cfb2ed3c54fef3e3dd3c36e3e877d978
mariamkhalifa <m_khalifa@fanhsaweonline.ca>
2020-01-21 16:51:04 -0500

    database fetch

--- js/main.js
+++ js/main.js
@@ -5,14 +5,10 @@ var vm = new Vue({
 
   data: {
 
-    user: {
-      isAdmin: true,
-      isLoggedIn: true,
-      avatar: "thor.png"
-    },
-
     // mock up the user - this well eventually come from the database UMS (user management system)
 
+    // this is an object
+    user: {},
 
     // this data would also come from the database, but we'll just mock it up for now
     videodata: [
@@ -21,39 +17,59 @@ var vm = new Vue({
       { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
     ],
 
-    videotitle: "Vidoe title goes here",
-    videodescription: "vid descritpion goes here",
-    videosource: "",
+    videotitle:"video title here",
+    videodescription: "vid desc here",
+    videosource:"",
 
     showDetails: false
   },
 
+  created: function() {
+    // vue instance is ready - mostly. Add some live data to the VM
+    this.fetchUsers();
+  },
+
   methods: {
     logInOut() {
-      // tst teh login / logout UI => button should change color
-      // eventually we'll rouing anda login component
-      console.log('do login / logout on click');
-
-      // ? : is a ternanry statement ( shorthand for if / else)
-      // evaluate the espression; if it's true. use the value to the
-      // left of teh semicolon. if it's false, sue the value to teh right
-      this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
+        // test the login/logout UI -> btn should change colour
+        // eventually we'll use routing and a login component
+        console.log('do login / logout on click');
+        // ? : ; = shorthand if statement - known as a ternary statement
+        // evaluate the expression; if true, use value on the left. If false, use value on right
+        this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
     },
 
     setUserPrefs() {
-      console.log('set user prefs via routing and probably a component');
+        console.log('set user prefs via routing and probably a component');
     },
 
-    // this is ES6 datadestructuting - pull the keys and values you need, no the whole object
-    loadMovie({name, description, vidsource}) {
-      console.log('show movie details');
+    loadMovie({name, description, vidsource}){
+        console.log('show movie details');
 
-      this.videotitle = name;
-      this.videodescription = description;
-      this.videosource = vidsource;
+        this.videotitle = name;
+        this.videodescription = description;
+        this.videosource = vidsource;
 
-      this.showDetails = true;
+        this.showDetails = true;
+    },
+
+    fetchUsers() {
+      // get user data here and push it back into the VM
+      console.log('fetch user data here');
+
+      const url = './includes/index.php?user=true';
+
+      //promise
+
+      fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+
+        // push our user data into the VM
+        this.user = data[0];
+      })
+      .catch((err) => console.log(err))
     }
-
   }
 });
